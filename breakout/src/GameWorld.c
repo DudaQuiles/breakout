14d50faa458a752197d7de2bbb08ae441a24b910
DudaQuiles <DudaQuilesFaculdade@gmail.com>
2026-06-29 02:36:51 -0700

    ldpafd

--- breakout/src/GameWorld.c
+++ breakout/src/GameWorld.c
@@ -68,7 +68,7 @@ GameWorld *createGameWorld(void){
         .cor = WHITE
     };
 
-    setarAlvos( GameWindow->gw );
+    setarAlvos( GameWorld->gw );
 
     return gw;
 
@@ -216,7 +216,7 @@ void gameOver(Jogador *j){
         j->vida = 3;
         j->pontuacao = 0;
         j->estado = 0;
-        setarAlvos( GameWindow->gw );
+        setarAlvos( GameWorld->gw );
     }
 }
 void resolverColisaoBolaJogador( Bolinha *b, Jogador *j ) {

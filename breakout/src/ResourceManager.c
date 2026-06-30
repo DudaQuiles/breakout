19bfb01af5e2d69c809f3d070e7ccb4b265c744f
DudaQuiles <DudaQuilesFaculdade@gmail.com>
2026-06-29 18:19:33 -0700

    kkkkkkkkkkkkkk

--- breakout/src/ResourceManager.c
+++ breakout/src/ResourceManager.c
@@ -17,7 +17,7 @@ ResourceManager rm = { 0 };
 void loadResourcesResourceManager( void ) {
     rm.textureExample = LoadTexture( "resources/images/mario.png" );
     rm.soundExample = LoadSound( "resources/sfx/powerUp.wav" );
-    rm.musicExample = LoadMusicStream( "resources/musics/overworld1.ogg" );
+    rm.musicExample = LoadMusicStream( "resources/musics/fcc-retro-arcade-game-music-297305.mp3" );
 }
 
 void unloadResourcesResourceManager( void ) {
